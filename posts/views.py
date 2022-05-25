5188a6dfe0bff72d70856c60561c06ed49d8e713
RA404 <Repin404@gmail.com>
2022-05-25 17:31:42 +0500

    first urls

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Main page')
+
+
+def country_posts(request):
+    return HttpResponse('Countries posts')
