2a46759ef24d47eb2268793dcc9383a0a369e8ad
RA404 <Repin404@gmail.com>
2022-06-04 15:33:52 +0500

    add password reset templates

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,10 @@ from django.contrib.auth.views import (
     LoginView,
     PasswordChangeView,
     PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
 )
 from django.urls import path
 from . import views
@@ -35,5 +39,33 @@ urlpatterns = [
             template_name='users/password_change_form.html'
         ),
         name='password_change',
+    ),
+    path(
+        'password_reset/done/',
+        PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
+        name='password_reset_done',
+    ),
+    path(
+        'password_reset/',
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+        ),
+        name='password_reset',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
+        name='password_reset_complete',
     ),
 ]
