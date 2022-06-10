8bfce60b3c28fcb3da347c8500f224b1d0075387
RA404 <Repin404@gmail.com>
2022-06-10 17:58:19 +0500

    add profile and post detail pages, fix static pages about and tech

--- about/views.py
+++ about/views.py
@@ -4,18 +4,6 @@ from django.views.generic.base import TemplateView
 class AboutAuthor(TemplateView):
     template_name = 'about/author.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_name'] = 'author'
-
-        return context
-
 
 class AboutTech(TemplateView):
     template_name = 'about/tech.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_name'] = 'tech'
-
-        return context
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpRequest
 from typing import Dict
-from .models import Post, Country
+from .models import Post, Country, User
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -31,5 +31,36 @@ def country_posts(request: HttpRequest, slug: str) -> HttpResponse:
         'page_posts': page_posts,
     }
     templates = 'posts/country_posts.html'
+
+    return render(request, templates, context)
+
+
+def profile(request: HttpRequest, user_name: str) -> HttpResponse:
+    user = get_object_or_404(User, username=user_name)
+    posts = Post.objects.filter(author=user)
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_posts = paginator.get_page(page_number)
+
+    context = {
+        'page_posts': page_posts,
+        'author': user,
+        'posts': posts,
+    }
+
+    templates = 'posts/profile.html'
+
+    return render(request, templates, context)
+
+
+def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post,
+    }
+
+    templates = 'posts/post_detail.html'
 
     return render(request, templates, context)
