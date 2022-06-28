4aea2e10a3c7521638802b42a61cd02e1ca8e77c
RA404 <Repin404@gmail.com>
2022-06-28 12:31:25 +0500

    add thumbnail app and pillow library with migration

--- travel_blog/settings.py
+++ travel_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
     'posts.apps.PostsConfig',
     'users.apps.UsersConfig',
     'core.apps.CoreConfig',
