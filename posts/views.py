4da61774a0c42a4bcb0d0ecef9838b0738082d47
RA404 <Repin404@gmail.com>
2022-07-22 23:15:08 +0500

    add css for posts

--- posts/views.py
+++ posts/views.py
@@ -86,12 +86,6 @@ def profile(request: HttpRequest, user_name: str) -> HttpResponse:
 def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
     post = get_object_or_404(Post, pk=post_id)
     comments = post.comments.all()
-    # likes = post.post_likes.count()
-    # already_like = False
-    # if request.user.is_authenticated:
-    #     already_like = Like.objects\
-    #         .filter(user=request.user, post=post)\
-    #         .exists()
 
     form = CommentForm(request.POST or None)
 
@@ -99,8 +93,6 @@ def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
         'post': post,
         'comments': comments,
         'form': form,
-        # 'likes': likes,
-        # 'already_like': already_like,
     }
 
     templates = 'posts/post_detail.html'
