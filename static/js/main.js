ad9fecdc53814f7095a14a8ace5663001a41f897
RA404 <Repin404@gmail.com>
2022-07-28 22:53:49 +0500

    fix js static console error

--- static/js/main.js
+++ static/js/main.js
@@ -2,13 +2,17 @@ const menuButton = document.getElementById('menu__toggle');
 const overlay = document.querySelector('.overlay');
 const sidebarMenu = document.querySelector('.sidebar-menu');
 
-menuButton.addEventListener('click', () => {
-    overlay.classList.toggle('overlay_active');
-    sidebarMenu.classList.toggle('sidebar-menu_active');
-});
+if (menuButton != null) {
+        menuButton.addEventListener('click', () => {
+        overlay.classList.toggle('overlay_active');
+        sidebarMenu.classList.toggle('sidebar-menu_active');
+    });
+}
 
-overlay.addEventListener('click', () => {
-    overlay.classList.remove('overlay_active');
-    sidebarMenu.classList.remove('sidebar-menu_active');
-    menuButton.checked = false;
-});
+if (overlay != null) {
+    overlay.addEventListener('click', () => {
+        overlay.classList.remove('overlay_active');
+        sidebarMenu.classList.remove('sidebar-menu_active');
+        menuButton.checked = false;
+    });
+}
